35b763ba013b4e59151d3bd9c39df210bef3ba3f
Marcus Comstedt <marcus@mc.pp.se>
2024-07-22 16:08:05 +0200

    Don't complain about unknown cti

--- wb_common.v
+++ wb_common.v
@@ -33,6 +33,7 @@ function wb_is_last;
 	CTI_CONST_BURST  : wb_is_last = 1'b0;
 	CTI_INC_BURST    : wb_is_last = 1'b0;
 	CTI_END_OF_BURST : wb_is_last = 1'b1;
+	3'bxxx           : wb_is_last = 1'bx;
 	default : $display("%d : Illegal Wishbone B3 cycle type (%b)", $time, cti);
       endcase
    end
